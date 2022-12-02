bdc34332e6fe817a464a70669a9e7c3f0483f537
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2022-12-01 21:54:11 -0300

    feat(chatbot): added test chatbot

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -8,7 +9,34 @@ export default function Home() {
         <title>Gabriel Dissotti</title>
         <meta name="description" content="Gabriel Dissotti Portfolio" />
         <link rel="icon" href="/favicon.ico" />
+
+        
       </Head>
+      
+      <Script
+        id="show-banner"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.addEventListener('mouseover', initLandbot, { once: true });
+window.addEventListener('touchstart', initLandbot, { once: true });
+var myLandbot;
+function initLandbot() {
+  if (!myLandbot) {
+    var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+    s.addEventListener('load', function() {
+      var myLandbot = new Landbot.Livechat({
+        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-1428860-ZSR3C18XQWPG1620/index.json',
+      });
+    });
+    s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  }
+}
+          
+          `,
+        }}
+      />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -55,9 +83,10 @@ export default function Home() {
             </ul>
           </li>
         </ul>
-
         
       </main>
+
+      
     </div>
   )
 }
